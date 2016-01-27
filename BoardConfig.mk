1e7f2955c63bdb65b2062f8b39d07ff72c3b592b
remicks <lost.junglist@gmail.com>
2016-01-27 14:06:15 -0800

    Stupid defconfig...

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -57,9 +57,9 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
 # Enable DIAG on debug builds
-ifneq ($(TARGET_BUILD_VARIANT),user)
-TARGET_KERNEL_ADDITIONAL_CONFIG ?= cyanogenmod_debug_config
-endif
+#ifneq ($(TARGET_BUILD_VARIANT),user)
+#TARGET_KERNEL_ADDITIONAL_CONFIG ?= cyanogenmod_debug_config
+#endif
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
