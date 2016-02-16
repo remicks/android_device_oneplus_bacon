e077ddd137970138ea35a58181026edcfd46e813
remicks <lost.junglist@gmail.com>
2016-02-16 09:24:27 -0800

    Remove DIAG section completely

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,11 +56,6 @@ TARGET_KERNEL_CONFIG := boeffla_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8974
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-eabi-
 
-# Enable DIAG on debug builds
-#ifneq ($(TARGET_BUILD_VARIANT),userdebug)
-#TARGET_KERNEL_ADDITIONAL_CONFIG ?= cyanogenmod_kerneldebug_config
-#endif
-
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
 
